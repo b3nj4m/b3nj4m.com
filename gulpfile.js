65d2a0f9aee18f18f8bf27ff6f18abef7ba4bd1c
Ben Robbins <ben.robbins@gmail.com>
2015-08-09 23:11:36 -0500

    redesign using materialize

--- gulpfile.js
+++ gulpfile.js
@@ -23,52 +23,16 @@ gulp.task('clean', function(done) {
 });
 
 gulp.task('styles', ['clean'], function() {
-  return gulp.src('dev/css/**/*')
+  return gulp.src(['node_modules/materialize-css/bin/*.css', 'dev/css/**/*'])
     .pipe(less())
     .pipe(concat('index.css'))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(autoprefixer('last 2 version', 'safari 6', 'ie 9', 'opera 12.1', 'ios 7', 'android 4'))
     .pipe(rev())
     .pipe(gulp.dest('build'))
 });
 
-//generate data used for header, sub-header media queries
-generateTmplData = function(data) {
-  var widths = _.range(data.width, data.minWidth, -data.widthStep);
-  var steps = [];
-
-  var headerSize = data.headerSize;
-  var subHeaderSize = data.subHeaderSize;
-  var headerSpacing = data.headerSpacing;
-  var subHeaderSpacing = data.subHeaderSpacing;
-
-  var headerSizeStep = (data.headerSize - data.minHeaderSize) / widths.length;
-  var subHeaderSizeStep = (data.subHeaderSize - data.minSubHeaderSize) / widths.length;
-  var headerSpacingStep = (data.headerSpacing - data.minHeaderSpacing) / widths.length;
-  var subHeaderSpacingStep = (data.subHeaderSpacing - data.minSubHeaderSpacing) / widths.length;
-
-  _.each(widths, function(width, idx) {
-    steps.push({
-      width: width,
-      headerSize: headerSize,
-      subHeaderSize: subHeaderSize,
-      headerSpacing: headerSpacing,
-      subHeaderSpacing: subHeaderSpacing
-    });
-
-    headerSize -= headerSizeStep;
-    subHeaderSize -= subHeaderSizeStep;
-    headerSpacing -= headerSpacingStep;
-    subHeaderSpacing -= subHeaderSpacingStep;
-  });
-
-  data.steps = steps;
-
-  return data;
-};
-
 gulp.task('html', ['clean'], function() {
   var tmplData = JSON.parse(fs.readFileSync('dev/json/index.json'));
-  tmplData = generateTmplData(tmplData);
 
   return gulp.src('dev/templates/**/*')
     .pipe(template(tmplData))
@@ -77,11 +41,17 @@ gulp.task('html', ['clean'], function() {
 
 gulp.task('static', ['clean'], function() {
   var keybaseFilter = filter('!keybase.txt');
-  return gulp.src('static/**/*')
+  return gulp.src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/materialize-css/bin/*.js', 'static/**/*'])
     .pipe(keybaseFilter)
     .pipe(rev())
     .pipe(keybaseFilter.restore())
     .pipe(gulp.dest('build'));
+});
+
+gulp.task('fonts', ['clean'], function() {
+  return gulp.src('node_modules/materialize-css/font/roboto/*')
+    .pipe(rev())
+    .pipe(gulp.dest('build/font/roboto'));
 });
 
 var hashedFileRegex = /-[a-z0-9]{8}\.[a-z]+$/i;
@@ -94,13 +64,13 @@ gulp.task('remove-unhashed', ['replace-urls'], function() {
     .pipe(clean());
 });
 
-gulp.task('replace-urls', ['styles', 'html', 'static'], function() {
+gulp.task('replace-urls', ['styles', 'html', 'static', 'fonts'], function() {
   var src = gulp.src('build/**/*.+(html|js|css)');
 
   glob.sync('build/**/*.+(!(html))').forEach(function(filePath) {
     if (fs.statSync(filePath).isFile()) {
       var replacePath = '/' + path.relative('build', filePath);
-      var origPath = replacePath.replace(/-[a-z0-9]{8}(\.[a-z]+)$/i, '$1');
+      var origPath = replacePath.replace(/-[a-z0-9]{8}(\.[a-z0-9]+)$/i, '$1');
       src = src.pipe(replace(origPath, replacePath));
     }
   });
@@ -131,5 +101,5 @@ gulp.task('deploy', ['build'], shell.task([
   'scp -r build/* b3nj4m@b3nj4m.com:~/b3nj4m.com/'
 ]));
 
-gulp.task('build', ['clean', 'styles', 'html', 'static', 'replace-urls']);
+gulp.task('build', ['clean', 'styles', 'html', 'static', 'fonts', 'replace-urls']);
 gulp.task('dev', ['build', 'serve', 'watch']);
